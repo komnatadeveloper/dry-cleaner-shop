715d68df9c83c17a4a71c95ad21280b3ad28ef98
komnata <ozyazgane@hotmail.com>
2020-08-10 17:09:52 +0300

    images for readmemd and some CSS improvements

--- client/src/components/Admin/Orders/PaymentItem.js
+++ client/src/components/Admin/Orders/PaymentItem.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
+import Moment from "react-moment"
 
 const PaymentItem = ({ payment }) => {
   useEffect(() => {}, [payment]);
@@ -8,11 +9,13 @@ const PaymentItem = ({ payment }) => {
 
   return (
     <tr>
-      <td>{date}</td>
+      <td>
+        <Moment format='YYYY-MM-DD HH:mm'>{date}</Moment>
+      </td>
       <td>{customerId.username}</td>
       {/* <td>{orderStatus}</td> */}
-      <td>{amount}</td>
-      <td>
+      <td className='font-weight-bold right-align pr-2'>{((-1) * amount).toFixed(2)}</td>
+      <td className='center-align'> 
         {/* <a class="waves-effect waves-light grey darken-1 btn-small mr-1">Edit</a> */}
         <Link
           to={`/dashboard/payments/edit/${_id}`}
